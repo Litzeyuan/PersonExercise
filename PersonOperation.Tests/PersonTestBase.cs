4115ed6758ceb96bc12d8a0c72b13174c7648bbc
lizeyuan <zeyuan.li.home@gmail.com>
2020-06-17 10:25:50 +0100

    Create test base class with seeded Person data

---
+++ PersonOperation.Tests/PersonTestBase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonOperation.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonOperation.Tests
+{
+    public class PersonTestBase
+    {
+        protected DbContextOptions<PersonDBContext> ContextOptions { get; }
+        protected PersonTestBase(DbContextOptions<PersonDBContext> contextOptions)
+        {
+            ContextOptions = contextOptions;
+    
+            Seed();
+        }
+
+
+        private void Seed()
+        {
+            var context = new PersonDBContext(ContextOptions);
+            context.Database.EnsureDeleted();
+
+            context.AddRange(
+                    new Person(111, "FirstName1", "LastName1"),
+                    new Person(112, "FirstName1", "LastName2")
+           );
+            context.SaveChanges();
+            
+
+        }
+    }
+}
