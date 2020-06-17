43054881c898597535aec5ba11f54a3de0805984
lizeyuan <zeyuan.li.home@gmail.com>
2020-06-17 10:19:07 +0100

    Create Person DBContext

---
+++ PersonOperation/Data/PersonDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonOperation.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonOperation.Data
+{
+    public interface IPersonDBContext
+    {
+        DbSet<Person> Person { get; set; }
+    }
+
+    public class PersonDBContext : DbContext, IPersonDBContext
+    {
+        public PersonDBContext(DbContextOptions<PersonDBContext> options) : base(options) { }
+        public DbSet<Person> Person { get; set; }
+    }
+}
